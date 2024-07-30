cfdb72ea38f44cd0663aa5028434b07f65c85cbe
Sherif <muhammedsherif40@yahoo.com>
2024-07-30 15:34:00 +0100

    added team section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Team from './components/Team'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
      <Navbar />
      <Hero />
+     <Team />
     </>
   )
 }
---
+++ src/components/Team.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Desc from '../assets/svg/desc.svg';
+import Gramm from '../assets/svg/gramm.svg';
+import Inter from '../assets/svg/inter.svg';
+import Notion from '../assets/svg/notion.svg';
+import Splash from '../assets/svg/splash.svg';
+
+const Team = () => {
+
+    const teams =[Splash, Notion, Inter, Desc, Gramm]
+
+  return (
+    <div className='w-full h-full px-4 lg:px-14 py-8 lg:py-6 bg-[#FFD6D6]'>
+        <h1 className='text-[2.5rem] text-[#191A15] font-bold text-center'>More than 25,000 teams use Collabs</h1>
+        <div className='w-full flex flex-wrap items-center gap-14 mt-8 mx-auto'>
+            {teams.map((items) => (
+                <img src={items} alt="" />
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default Team
