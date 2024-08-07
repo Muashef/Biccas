3f31ae615a869ab144a8d95ca8fa4886efe0326b
Sherif <muhammedsherif40@yahoo.com>
2024-08-07 23:38:02 +0100

    added benefits page

--- src/components/Benefit.jsx
+++ src/components/Benefit.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Lap from '../assets/svg/laptop.svg';
+import Checks from '../assets/svg/check.svg';
 
 const Benefit = () => {
   return (
@@ -7,6 +8,28 @@ const Benefit = () => {
         <div className='flex flex-col lg:flex-row max-w-[80vw] lg:max-w-7xl mx-auto'>
             <div>
                 <h1 className='text-[#191A15] font-extrabold text-[50px]'>What Benifit Will You Get</h1>
+                <div className='w-full flex flex-col space-y-8 mt-10'>
+                  <div className='flex items-center gap-3'>
+                    <img src={Checks} alt="Checks" />
+                    <p className='text-[#191A15] text-lg font-medium'>Free Consulting With Experet Saving Money</p>
+                  </div>
+                  <div className='flex items-center gap-3'>
+                    <img src={Checks} alt="Checks" />
+                    <p className='text-[#191A15] text-lg font-medium'>Online Banking</p>
+                  </div>
+                  <div className='flex items-center gap-3'>
+                    <img src={Checks} alt="Checks" />
+                    <p className='text-[#191A15] text-lg font-medium'>Investment Report Every Month</p>
+                  </div>
+                  <div className='flex items-center gap-3'>
+                    <img src={Checks} alt="Checks" />
+                    <p className='text-[#191A15] text-lg font-medium'>Saving Money For The Future</p>
+                  </div>
+                  <div className='flex items-center gap-3'>
+                    <img src={Checks} alt="Checks" />
+                    <p className='text-[#191A15] text-lg font-medium'>Online Transaction</p>
+                  </div>
+                </div>
             </div>
             <div>
                 <img src={Lap} alt="laptop" />
