f62c4cce1294be70e3961a83e365e9ffd434b50d
Sherif <muhammedsherif40@yahoo.com>
2024-08-08 23:34:50 +0100

    plan page in progress

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Plan = () => {
   return (
     <div className='w-full h-full px-14 py-6'>
-        <h1 className='text-[#191A15] font-bold text-[50px] text-center '>Choose Plan That's Right for you</h1>
+        <h1 className='text-[#191A15] font-bold text-[50px] text-center '>Choose Plan <br /> That's Right for you</h1>
+        <p className='text-[#A6A6A6] text-lg font-bold mt-8 text-center'>Choose plan that works best for you, feel free to contact us</p>
+        <div className='flex flex-row items-center justify-center mt-4 bg-white p-5 border border-white rounded-lg'>
+            <button>Bill Monthly</button>
+            <button className='bg-[#54BD95] py-3 px-4 rounded-lg text-white text-lg'>Bill Yearly</button>
+        </div>
     </div>
   )
 }
