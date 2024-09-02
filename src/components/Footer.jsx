79caa3ab73823e7918b4adc9d0b927071ce30985
Sherif <muhammedsherif40@yahoo.com>
2024-09-02 18:20:14 +0100

    footer in progress

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,20 @@
 import React, {useState} from 'react'
 
 const Footer = () => {
-    const [isOn, setIsOn] = useState(false);
-
-    const handleToggle = () => {
-        setIsOn(!isOn)
-    }
+   
   return (
-    <div className={`flex items-center cursor-pointer w-16 p-1 rounded-full ${
-        isOn ? 'bg-blue-500' : 'bg-gray-300'
-    }`}
-        onClick={handleToggle}
-    >
-    <div
-      className={ `h-6 w-6 rounded-full bg-white shadow-md transform duration-300 ease-in-out ${
-        isOn ? 'translate-x-6' : ''
-      }`}
-    >
-
+    <div className='w-full h-full py-10 px-14 bg-[#161C28]'>
+        <div className='flex flex-col md:flex-row items-center justify-between '>
+            <div>
+                <h2 className='text-lg font-medium text-white'>© 2022 Biccas Inc. Copyright and rights reserved</h2>
+            </div>
+            <div>
+                <p className='text-lg font-medium text-white'>Terms and Condtions</p>
+            </div>
+        </div>
     </div>
-    </div>
+    
+    
   )
 }
 
